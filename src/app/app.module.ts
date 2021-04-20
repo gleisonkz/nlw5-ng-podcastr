27ceb4b53efcdd465b614659b7916069391b5932
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2021-04-20 11:31:32 -0300

    ✨ feat: player component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSliderModule } from '@angular/material/slider';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { PlayerComponent } from './components/player/player.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, PlayerComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -17,6 +22,10 @@ import { HeaderComponent } from './components/header/header.component';
     MatSidenavModule,
     MatToolbarModule,
     MatDividerModule,
+    MatProgressBarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSliderModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
