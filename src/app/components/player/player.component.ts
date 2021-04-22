eff7c148019bcd0c133fb760370ba7ef21a6746b
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2021-04-22 17:48:47 -0300

    📦 refactor: podcast time format

--- src/app/components/player/player.component.ts
+++ src/app/components/player/player.component.ts
@@ -10,17 +10,22 @@ import { PlayerEpisode, PlayerService } from 'src/app/services/player.service';
 export class PlayerComponent implements OnInit {
   currentEpisode: PlayerEpisode;
   isPlaying$: Observable<boolean>;
+  timeElapsed$: Observable<string>;
+  currentDuration$: Observable<number>;
 
   constructor(private playerService: PlayerService) {}
 
   ngOnInit(): void {
     this.isPlaying$ = this.playerService.isPlaying$;
+    this.timeElapsed$ = this.playerService.timeElapsed$;
+    this.currentDuration$ = this.playerService.duration$;
     this.playerService.currentEpisode$.subscribe((episode) => {
       this.currentEpisode = episode;
     });
   }
 
   togglePlay() {
+    if (!this.currentEpisode) return;
     this.playerService.togglePlay();
   }
 }
