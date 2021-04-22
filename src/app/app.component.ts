f5e076e8134bcf945adfe7259e680b9fbdeeea5a
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2021-04-22 10:19:21 -0300

    ✨ feat: episode-detail component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'pd-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'ng-podcastr';
-}
+export class AppComponent {}
