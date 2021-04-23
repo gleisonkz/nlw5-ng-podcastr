79d5ab7e0da3660dbfab64555f537e8abb30f722
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2021-04-23 01:15:22 -0300

    ✨ feat: sliderThumbChange

--- src/app/components/player/player.component.ts
+++ src/app/components/player/player.component.ts
@@ -1,31 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { PlayerEpisode, PlayerService } from 'src/app/services/player.service';
+import { Component } from '@angular/core';
+import { MatSliderChange } from '@angular/material/slider';
+import { PlayerService } from 'src/app/services/player.service';
 
 @Component({
   selector: 'pd-player',
   templateUrl: './player.component.html',
   styleUrls: ['./player.component.scss'],
 })
-export class PlayerComponent implements OnInit {
-  currentEpisode: PlayerEpisode;
-  isPlaying$: Observable<boolean>;
-  timeElapsed$: Observable<string>;
-  currentDuration$: Observable<number>;
+export class PlayerComponent {
+  currentEpisode$ = this.playerService.currentEpisode$;
+  isPlaying$ = this.playerService.isPlaying$;
+  timeElapsed$ = this.playerService.timeElapsed$;
+  currentDuration$ = this.playerService.duration$;
 
   constructor(private playerService: PlayerService) {}
 
-  ngOnInit(): void {
-    this.isPlaying$ = this.playerService.isPlaying$;
-    this.timeElapsed$ = this.playerService.timeElapsed$;
-    this.currentDuration$ = this.playerService.duration$;
-    this.playerService.currentEpisode$.subscribe((episode) => {
-      this.currentEpisode = episode;
-    });
+  sliderThumbChange(event: MatSliderChange) {
+    if (!event.value) return;
+    this.playerService.seekAudio(event.value);
   }
 
   togglePlay() {
-    if (!this.currentEpisode) return;
     this.playerService.togglePlay();
   }
 }
--- src/app/services/player.service.ts
+++ src/app/services/player.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable, Subject } from 'rxjs';
+import { BehaviorSubject, Subject } from 'rxjs';
 import { Episode } from 'src/app/models/episode.model';
 import { mapEpisodeToPlayerEpisode } from 'src/app/utils/mapPlayerEpisode';
 
@@ -21,8 +21,8 @@ export class PlayerService {
   private episodes$$ = new Subject<PlayerEpisode[]>();
   private currentEpisode$$ = new Subject<PlayerEpisode>();
   private isPlaying$$ = new BehaviorSubject<boolean>(false);
-  private timeElapsed$$ = new BehaviorSubject<string>('00:00');
-  private duration$$ = new BehaviorSubject<number>(0);
+  private timeElapsed$$ = new BehaviorSubject<number>(0);
+  private duration$$ = new BehaviorSubject<number>(0.1);
 
   public episodes$ = this.episodes$$.asObservable();
   public currentEpisode$ = this.currentEpisode$$.asObservable();
@@ -37,7 +37,6 @@ export class PlayerService {
 
   private calculateTime = () => {
     let currentTime = this.audio.currentTime;
-    let duration = this.audio.duration;
     this.setTimeElapsed(currentTime);
   };
 
@@ -71,6 +70,19 @@ export class PlayerService {
     this.audio.addEventListener('ended', this.setPlayerStatus, false);
   }
 
+  private setAudioUrl(src: string): void {
+    this.audio.src = src;
+    this.playAudio();
+  }
+
+  private setTimeElapsed(currentTimeInSeconds: number): void {
+    this.timeElapsed$$.next(Math.floor(currentTimeInSeconds));
+  }
+
+  private playAudio(): void {
+    this.audio.play();
+  }
+
   setInitialValue(episodes: PlayerEpisode[]) {
     this.episodes$$.next(episodes);
   }
@@ -89,15 +101,6 @@ export class PlayerService {
     this.isPlaying$$.next(!this.isPlaying$$.value);
   }
 
-  setAudioUrl(src: string): void {
-    this.audio.src = src;
-    this.playAudio();
-  }
-
-  playAudio(): void {
-    this.audio.play();
-  }
-
   pauseAudio(): void {
     this.audio.pause();
   }
@@ -108,18 +111,6 @@ export class PlayerService {
 
   seekAudio(position: number): void {
     this.audio.currentTime = position;
-  }
-
-  private setTimeElapsed(ct: number): void {
-    let seconds = Math.floor(ct % 60),
-      displaySecs = seconds < 10 ? '0' + seconds : seconds,
-      minutes = Math.floor((ct / 60) % 60),
-      displayMins = minutes < 10 ? '0' + minutes : minutes;
-
-    this.timeElapsed$$.next(displayMins + ':' + displaySecs);
-  }
-
-  public getPlayerStatus(): Observable<string> {
-    return this.playerStatus.asObservable();
+    this.setTimeElapsed(position);
   }
 }
