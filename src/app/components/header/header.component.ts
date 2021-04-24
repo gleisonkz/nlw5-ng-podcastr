c8e044c5432187dc3d546917ba797222e9f8a544
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2021-04-24 14:50:36 -0300

    ✨ feat: mobile menu

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'pd-header',
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   currentDate = new Date();
+  @Output() openSide = new EventEmitter<void>();
 }
