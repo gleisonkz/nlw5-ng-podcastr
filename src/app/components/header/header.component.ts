ea53b27b4dde5f867ef8ff8676dff8fc01927991
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2021-07-02 15:20:27 -0300

    📦 refactor: remove unnecessary code

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,22 +1,18 @@
 import { DOCUMENT } from '@angular/common';
-import { Component, EventEmitter, Inject, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Inject, Output } from '@angular/core';
 
 @Component({
   selector: 'pd-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
   constructor(@Inject(DOCUMENT) private ngDocument: Document) {}
 
   currentDate = new Date();
   isChecked = false;
   @Output() openSide = new EventEmitter<void>();
   toggleTheme() {
-    this.ngDocument.documentElement.classList.toggle('dark-mode');
-  }
-
-  ngOnInit(): void {
     this.ngDocument.documentElement.classList.toggle('dark-mode');
   }
 }
