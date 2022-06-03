f06ba0811d07d90c08d9f69db5872d8536fbd49a
Gleison de Almeida <gleisonsubzerokz@gmail.com>
2022-06-03 02:29:04 -0300

    :package: refactor: remove flex-layout package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
+import { EPISODE_SERVICE_TOKEN, tokenServiceFactory } from 'src/app/services/episode-service-token';
+import { EpisodeStorageService } from 'src/app/services/episode-storage.service';
+import { EpisodeService } from 'src/app/services/episode.service';
+
 import { DatePipe, registerLocaleData } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import ptBr from '@angular/common/locales/pt';
 import { LOCALE_ID, NgModule } from '@angular/core';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
@@ -16,12 +19,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {
-  EPISODE_SERVICE_TOKEN,
-  tokenServiceFactory,
-} from 'src/app/services/episode-service-token';
-import { EpisodeStorageService } from 'src/app/services/episode-storage.service';
-import { EpisodeService } from 'src/app/services/episode.service';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -58,11 +56,9 @@ registerLocaleData(ptBr);
     MatIconModule,
     MatSliderModule,
     MatTooltipModule,
-
     HttpClientModule,
     MatTableModule,
     MatTabsModule,
-    FlexLayoutModule,
   ],
   providers: [
     { provide: LOCALE_ID, useValue: 'pt-BR' },
